696e2d2b9f9739307bc178cba317785889feba8b
jiahao <j.gong6.@icloud.com>
2020-03-16 01:13:35 +0800

    :zap: shell命令获取podsec的文件名减少fastlane 脚本参数

--- autoPodRelease.sh
+++ autoPodRelease.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 tagVersion=$1
 tagMessage=$2
-projectName="<#这里替换成your_podsec_Name#>" #私有组件名
-repoName="YGSpecs" #私有pod库名
 
 echo "....切换到fastlane的同级目录..."
 cd .. 
 pwd  #显示当前目录
-echo "....一键发布私有库..."
 
-fastlane JHUpdatePodTool project:"${projectName}" version:"${tagVersion}" tagMsg:"${tagMessage}" repo:"${repoName}" 
-#fastlane JHUpdatePodTool project:"SDCycleScrollView" version:"1.80.0" tagMsg:"基于github版本的1.80修改 by fastlan Auto" repo:"YGSpecs" 
+podSecPath=`find ./ -name '*.podspec' -print`
+
+echo "podspec的路径是:$podSecPath"
+
+#echo ${podSecPath##*/} 
+projectNameTemp=`echo ${podSecPath##*/}` #私有组件文件名
+projectName=`echo ${projectNameTemp%%.*}` #文件名.type 取文件名
+echo "podsecs是$projectName"
+#
+repoName="YGSpecs" #私有pod库名,根据需要修改
+
+echo "....一键发布私有库..."
+if [ ! $tagMessage ];then
+#	echo "tagMessage为空"
+	fastlane JHUpdatePodTool project:"${projectName}" version:"${tagVersion}"  repo:"${repoName}"  #tagMsg:"${tagMessage}"
+	#fastlane JHUpdatePodTool project:"SDCycleScrollView" version:"1.80.0" tagMsg:"基于github版本的1.80修改 by fastlan Auto" repo:"YGSpecs" 
+else
+#	echo "tagMessage是:${tagMessage}"
+	fastlane JHUpdatePodTool project:"${projectName}" version:"${tagVersion}" tagMsg:"${tagMessage}" repo:"${repoName}" 
+fi
